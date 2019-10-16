d7997bfb0f907db5e8010b2430d760e9ac193d32
JorgeInfante1 <jorge_infante90@hotmail.com>
2019-10-16 15:58:23 -0500

    Cat class created

--- src/com/exercise11oop/app/PerroApp.java
+++ src/com/exercise11oop/app/PerroApp.java
@@ -1,25 +1,30 @@
 package com.exercise11oop.app;
 
-import com.exercise11oop.model.Perro;
+import com.exercise11oop.model.Dog;
 public class PerroApp 
 {
 
 	public static void main(String[] args) 
 	{
 	    //Way
-		Perro pluto; 
-		pluto=new Perro();
+		Dog pluto; 
+		pluto=new Dog();
 		
 		//Way 2
-		Perro tribilin=new Perro();
 		
 		pluto.age=15;
-		pluto.heigth=50;
+		pluto.height=50;
 		pluto.id=10;
 		
 		System.out.println("Puto's Age: "+pluto.age);
-		System.out.println("Pluto's Height: "+pluto.heigth);
+		System.out.println("Pluto's Height: "+pluto.height);
 		System.out.println("Pluto's ID: "+pluto.id);
+		
+		Dog firulais=new Dog(1, "Firulais","Labrador",5,60.0);
+		
+		System.out.println("Firulais age: "+firulais.age);
+		System.out.println("Firulais height"+firulais.height);
+		System.out.println("Firulaius ID:"+firulais.id);
 	}
 
 }
